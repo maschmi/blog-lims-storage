c0412cb6b8fb2eaa4bf16a5f09bd9596983d7196
maschmi <maschmi@maschmi.net>
2024-06-02 12:01:55 +0200

    adds kotest to spring boot backend

--- spring-boot-backend/build.gradle.kts
+++ spring-boot-backend/build.gradle.kts
@@ -20,7 +20,11 @@ repositories {
 	mavenCentral()
 }
 
-dependencies {
+dependencies{
+	val koTestVersion: String by properties
+	val koTestSpringVersion: String by properties
+	val mockkVersion: String by properties
+
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
@@ -32,6 +36,11 @@ dependencies {
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	testImplementation("io.kotest:kotest-runner-junit5:$koTestVersion")
+	testImplementation("io.kotest:kotest-assertions-core:$koTestVersion")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:$koTestSpringVersion")
+	testImplementation("io.mockk:mockk:${mockkVersion}")
 }
 
 kotlin {
--- spring-boot-backend/settings.gradle.kts
+++ spring-boot-backend/settings.gradle.kts
@@ -1,1 +1,18 @@
+pluginManagement {
+    val springBootVersion: String by settings
+    val springBootDependencyManagement: String by settings
+    val kotlinVersion: String by settings
+    repositories {
+        gradlePluginPortal()
+    }
+    plugins {
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version springBootDependencyManagement
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.spring") version kotlinVersion
+        kotlin("plugin.jpa") version kotlinVersion
+    }
+}
+
+
 rootProject.name = "spring-boot-backend"
