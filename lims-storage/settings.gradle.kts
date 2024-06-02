96f37f9a8f6157c59cdffeb74cb0ed18e6894eb7
maschmi <maschmi@maschmi.net>
2024-06-02 11:43:29 +0200

    updates gradle and dependencies

--- lims-storage/settings.gradle.kts
+++ lims-storage/settings.gradle.kts
@@ -1,3 +1,12 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    repositories {
+        gradlePluginPortal()
+    }
+    plugins {
+     kotlin("jvm") version kotlinVersion
+    }
+}
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
