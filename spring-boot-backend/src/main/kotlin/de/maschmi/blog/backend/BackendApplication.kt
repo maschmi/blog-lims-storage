7f6f74a8b4267a32926ba51fa39dbc02272a895e
maschmi <maschmi@maschmi.net>
2024-06-16 12:59:46 +0200

    adds application runner as test

--- spring-boot-backend/src/main/kotlin/de/maschmi/blog/backend/BackendApplication.kt
+++ spring-boot-backend/src/main/kotlin/de/maschmi/blog/backend/BackendApplication.kt
@@ -1,11 +1,38 @@
 package de.maschmi.blog.backend
 
+import application.CommissionStorageCabinet
+import application.GetListOfStorageCabinets
+import domain.Room
+import domain.RoomName
+import domain.StorageCabinetName
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
+import kotlin.system.exitProcess
 
 @SpringBootApplication
 class BackendApplication
 
 fun main(args: Array<String>) {
 	runApplication<BackendApplication>(*args)
+}
+
+
+@Component
+class AppStartupRunner(
+    private val commissionStorageCabinet: CommissionStorageCabinet,
+    private val getListOfStorageCabinets: GetListOfStorageCabinets
+) : ApplicationRunner {
+    override fun run(args: ApplicationArguments) {
+        println("Hello from AppRunner")
+        commissionStorageCabinet.execute(
+			name = StorageCabinetName("test"),
+			room = Room(RoomName("test-room"))
+		)
+        val cabinets = getListOfStorageCabinets.execute()
+        println("List of Storage Cabinets: $cabinets")
+        exitProcess(0)
+    }
 }
